fa21842b58de575a15e4357ecd3c4971d04d7a09
tux-mind <massimo.dragano@gmail.com>
2015-11-26 13:16:19 +0100

    statically import openssl, fix android 6

--- Android.mk
+++ Android.mk
@@ -71,7 +71,8 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/lib/
 LOCAL_C_INCLUDES += external/openssl/include
 LOCAL_CFLAGS += $(common_CFLAGS)
 
-LOCAL_SHARED_LIBRARIES := libssl libcrypto libz
+LOCAL_SHARED_LIBRARIES := libz
+LOCAL_STATIC_LIBRARIES:= libcrypto_static libssl
 
 LOCAL_COPY_HEADERS_TO := libcurl/curl
 LOCAL_COPY_HEADERS := $(addprefix include/curl/,$(CURL_HEADERS))
